8fac5512ab0f226f63988c21a26184bfff755172
VirginiSTR <vtorrealba@correo.unimet.edu.ve>
2024-03-23 23:16:36 -0400

    Creación de Nodo2

---
+++ src/Structure/Nodo2.java
@@ -0,0 +1,64 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Structure;
+
+/**
+ *
+ * @author VirginiaT
+ */
+
+public class Nodo2 <T> {
+    
+    T dato;
+    
+    
+    Nodo2 left;
+    
+    
+    Nodo2 right;
+
+   
+    public Nodo2(T dato) {
+        this.dato = dato;
+        this.right = null;
+        this.left = null;
+    }
+
+  
+    public T getDato() {
+        return dato;
+    }
+
+    
+    public void setDato(T dato) {
+        this.dato = dato;
+    }
+
+    
+    public Nodo2 getLeft() {
+        return left;
+    }
+
+    
+    public void setLeft(Nodo2 left) {
+        this.left = left;
+    }
+
+    
+    public Nodo2 getRight() {
+        return right;
+    }
+
+    
+    public void setRight(Nodo2 right) {
+        this.right = right;
+    }
+
+    
+    public boolean isLeaf() {
+        return (this.left == null && this.right == null);
+    }
+    
+}
